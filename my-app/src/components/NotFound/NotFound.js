98469892454ba82dd0ca3aac6b9670e235c54229
tanovik <taisianovikova@gmail.com>
2023-03-01 12:40:34 +0300

    folders reorganized

---
+++ my-app/src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="notfound_wrapper">
+      <div className="notfound_text_wrapper">
+        <h1>404</h1>
+        <br />
+        <h3>SORRY, PAGE NOT FOUND</h3>
+      </div>
+    </div>
+  );
+};
+export default NotFound;
